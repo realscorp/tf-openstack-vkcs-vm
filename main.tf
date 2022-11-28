7bdb21d271a7763c25400c75a016e64fec16f90c
Sergey Krasnov <35955067+realscorp@users.noreply.github.com>
2022-11-28 11:09:06 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     required_providers {
         openstack = {
         source = "terraform-provider-openstack/openstack"
-        version = "1.33.0"
+        version = ">= 1.33.0"
         }
     }
 }
